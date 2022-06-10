2c96f738acfc4580656d399241741689ef2e051a
Aizada Turarova <52201981+WebAiz@users.noreply.github.com>
2022-06-10 11:21:03 +0600

    Complete blog styling

--- src/app/pages/Blog/index.tsx
+++ src/app/pages/Blog/index.tsx
@@ -1,12 +1,20 @@
 // @flow
 import * as React from 'react';
+import './Blog.scss';
 
 type Props = {};
 
 export function Blog(props: Props) {
+  const images = new Array(8).fill(0);
   return (
       <main className={'blog'}>
-        fds
+        <h1>TITLE</h1>
+        <p className={'mb'}>Disda sdfsd f fsdfsd s fsdf </p>
+        <div className={'blog-list'}>
+          {images.map((el, i) => (
+              <img src="/images/map.png" alt="img" />
+          ))}
+        </div>
       </main>
   );
 }
